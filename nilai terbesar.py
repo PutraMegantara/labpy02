38d86dcc54b7a5500c44a24b778fa3bd295c4a76
PutraMegantara <92736847+PutraMegantara@users.noreply.github.com>
2021-11-15 20:07:31 +0700

    Add files via upload

---
+++ nilai terbesar.py
@@ -0,0 +1,12 @@
+
+a = int(input('masukan nilai a : '))
+b = int(input('masukan nilai a : '))
+c = int(input('masukan nilai a : '))
+
+if a > b and a > c:
+    print ('nilai yang tertinggi', a)
+elif b > a and b > c:
+    print ('nilai yang tertinggi', b)
+else:
+    print ('nilai yang tertinggi', c)
+        
